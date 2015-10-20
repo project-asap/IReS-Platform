a7e0b28496f2af97ed347b71377d4dde9f17d43d
Victor Giannakouris <victorasgs@gmail.com>
2015-10-20 12:49:26 +0300

    ML Tools, totalError Method

---
+++ asap-platform/asap-beans/src/main/java/gr/ntua/cslab/asap/optimization/ML.java
@@ -0,0 +1,22 @@
+package gr.ntua.cslab.asap.optimization;
+
+import gr.ntua.ece.cslab.panic.core.containers.beans.OutputSpacePoint;
+import gr.ntua.ece.cslab.panic.core.models.Model;
+
+import java.util.List;
+
+public class ML {
+    public static double totalError(Model model){
+        List<OutputSpacePoint> actualPoints = model.getOriginalPointValues();
+        double totalError = 0.0;
+        double predicted;
+        for (OutputSpacePoint point : actualPoints){
+            try{
+                predicted = model.getPoint(point.getInputSpacePoint()).getValue();
+                totalError += Math.abs(point.getValue() - predicted);
+            }
+            catch(Exception e){ continue; }
+        }
+        return totalError;
+    }
+}
