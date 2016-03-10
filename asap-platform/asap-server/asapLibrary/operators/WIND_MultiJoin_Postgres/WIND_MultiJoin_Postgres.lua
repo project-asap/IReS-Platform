5670592ac2a48a33838675228bd4f817f05ebeac
Papaioannou Vassilis <vpapa@cslab.ece.ntua.gr>
2016-03-10 15:58:27 +0200

    Update big workflows for wind

--- asap-platform/asap-server/asapLibrary/operators/WIND_MultiJoin_Postgres/WIND_MultiJoin_Postgres.lua
+++ asap-platform/asap-server/asapLibrary/operators/WIND_MultiJoin_Postgres/WIND_MultiJoin_Postgres.lua
@@ -1,21 +1,32 @@
-SHELL_COMMAND = "./WIND_MultiJoin_Postgres.sh"
+-- General configuration of the operators belonging to Wind_Demo_o_Postgres workflow
+BASE = "${JAVA_HOME}/bin/java -Xms64m -Xmx128m com.cloudera.kitten.appmaster.ApplicationMaster"
+TIMEOUT = -1
+MEMORY = 1024
+CORES = 1
+LABELS = "postgres"
+EXECUTION_NODE_LOCATION = "hdp1"
+
+-- Specific configuration of operator
+OPERATOR = "WIND_MultiJoin_Postgres"
+SCRIPT = OPERATOR .. ".sh"
+SHELL_COMMAND = "./" .. SCRIPT
 -- The actual distributed shell job.
 operator = yarn {
-  name = "Execute Java Operator",
-  timeout = -1,
-  memory = 1024,
-  cores = 1,
-  labels = "postgres",
-  nodes = "hdp1.itc.unipi.it",
-  master = {
+  name 		= "Execute " .. OPERATOR .. " Operator",
+  timeout 	= TIMEOUT,
+  memory 	= MEMORY,
+  cores 	= CORES,
+  labels 	= LABELS,
+  nodes 	= EXECUTION_NODE_LOCATION,
+  master 	= {
     env = base_env,
     resources = base_resources,
     command = {
-      base = "${JAVA_HOME}/bin/java -Xms64m -Xmx128m com.cloudera.kitten.appmaster.ApplicationMaster",
+      base = BASE,
       args = { "-conf job.xml" },
     }
   },
-
+  
   container = {
     instances = CONTAINER_INSTANCES,
     env = base_env,
