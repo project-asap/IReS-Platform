d0f5ce153ff1561d814e03d9b2f60eb1ffd1ba14
Papaioannou Vassilis <vpapa@cslab.ece.ntua.gr>
2016-03-13 14:02:29 +0200

    Update big workflows for wind

--- asap-platform/asap-server/asapLibrary/operators/WIND_Join_test_VORONOI_Postgres/WIND_Join_test_VORONOI_Postgres.sh
+++ asap-platform/asap-server/asapLibrary/operators/WIND_Join_test_VORONOI_Postgres/WIND_Join_test_VORONOI_Postgres.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 sudo -u postgres psql $5 -c "DROP TABLE $4; "
-sudo -u postgres psql $5 -c "CREATE TABLE $4 AS SELECT * FROM $1 LEFT JOIN $2 ON $3 ;"
+sudo -u postgres psql $5 -c "CREATE TABLE $4 AS SELECT * FROM $1 LEFT JOIN $2 USING $3 ;"
