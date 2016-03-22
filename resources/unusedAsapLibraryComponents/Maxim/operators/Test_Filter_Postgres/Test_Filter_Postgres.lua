813690349865e0ecd3271c062e5fe0fc3a5cf7ea
Papaioannou Vassilis <vpapa@cslab.ece.ntua.gr>
2016-03-22 23:53:51 +0200

    Remove some unneeded operators

---
+++ resources/unusedAsapLibraryComponents/Maxim/operators/Test_Filter_Postgres/Test_Filter_Postgres.lua
@@ -0,0 +1,48 @@
+SHELL_COMMAND = "./Test_Filter_Postgres.sh"
+CONTAINER_INSTANCES = 1
+MASTER_JAR_LOCATION = "kitten-master-0.2.0-jar-with-dependencies.jar"
+
+CP = "/opt/hadoop-2.7.0/etc/hadoop:/opt/hadoop-2.7.0/etc/hadoop:/opt/hadoop-2.7.0/etc/hadoop:/opt/hadoop-2.7.0/share/hadoop/common/lib/*:/opt/hadoop-2.7.0/share/hadoop/common/*:/opt/hadoop-2.7.0/share/hadoop/hdfs:/opt/hadoop-2.7.0/share/hadoop/hdfs/lib/*:/opt/hadoop-2.7.0/share/hadoop/hdfs/*:/opt/hadoop-2.7.0/share/hadoop/yarn/lib/*:/opt/hadoop-2.7.0/share/hadoop/yarn/*:/opt/hadoop-2.7.0/share/hadoop/mapreduce/lib/*:/opt/hadoop-2.7.0/share/hadoop/mapreduce/*:/contrib/capacity-scheduler/*.jar:/opt/hadoop-2.7.0/share/hadoop/yarn/*:/opt/hadoop-2.7.0/share/hadoop/yarn/lib/*"
+
+-- Resource and environment setup.
+base_resources = {
+  ["master.jar"] = { file = MASTER_JAR_LOCATION }
+}
+base_env = {
+  CLASSPATH = table.concat({"${CLASSPATH}", CP, "./master.jar", "./Test_Filter_Postgres.sh"}, ":"),
+}
+
+-- The actual distributed shell job.
+operator = yarn {
+  name = "Execute Java Operator",
+  timeout = -1,
+  memory = 1024,
+  cores = 1,
+  labels = "postgres",
+  nodes = "hdp1",
+  master = {
+    env = base_env,
+    resources = base_resources,
+    command = {
+      base = "${JAVA_HOME}/bin/java -Xms64m -Xmx128m com.cloudera.kitten.appmaster.ApplicationMaster",
+      args = { "-conf job.xml" },
+    }
+  },
+
+  container = {
+    instances = CONTAINER_INSTANCES,
+    env = base_env,
+    stageout = {"output"},
+    resources = {
+    ["Test_Filter_Postgres.sh"] = {
+       file = "/opt/asap-server/asapLibrary/operators/Test_Filter_Postgres/Test_Filter_Postgres.sh",
+      type = "file",               -- other value: 'archive'
+      visibility = "application",  -- other values: 'private', 'public'
+  }
+    },
+    command = {
+  base = SHELL_COMMAND,
+--  args = { "1> <LOG_DIR>/stdout", "2> <LOG_DIR>/stderr" },
+    }
+  }
+}
