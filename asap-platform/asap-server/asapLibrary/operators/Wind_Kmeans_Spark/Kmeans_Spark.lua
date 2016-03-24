cd21b3d6b568518e115d64d46b7986985b574ba1
Papaioannou Vassilis <vpapa@cslab.ece.ntua.gr>
2016-03-24 13:15:24 +0200

    Add alternative to Wind_Sociometer workflow

---
+++ asap-platform/asap-server/asapLibrary/operators/Wind_Kmeans_Spark/Kmeans_Spark.lua
@@ -0,0 +1,56 @@
+-- General configuration of the operators belonging to Wind_Demo_o_Postgres workflow
+BASE = "${JAVA_HOME}/bin/java -Xms64m -Xmx128m com.cloudera.kitten.appmaster.ApplicationMaster"
+TIMEOUT = 1000000000
+MEMORY = 1024
+CORES = 1
+EXECUTION_NODE_LOCATION = "hdp1"
+OPERATOR_LIBRARY = "asapLibrary/operators"
+
+-- Specific configuration of operator
+OPERATOR = "Wind_Kmeans_Spark"
+SCRIPT = OPERATOR .. ".sh"
+SHELL_COMMAND = "./" .. SCRIPT
+-- Home directory of operator
+OPERATOR_HOME = OPERATOR_LIBRARY .. "/" .. OPERATOR
+
+-- The actual distributed shell job.
+operator = yarn {
+  name 		= "Execute " .. OPERATOR .. " Operator",
+  timeout 	= TIMEOUT,
+  memory 	= MEMORY,
+  cores 	= CORES,
+  nodes 	= EXECUTION_NODE_LOCATION,
+  master 	= {
+    env = base_env,
+    resources = base_resources,
+    command = {
+      base = BASE,
+      args = { "-conf job.xml" },
+    }
+  },
+
+	container = {
+    		instances = CONTAINER_INSTANCES,
+    		env = base_env,
+    		command = {
+				base = SHELL_COMMAND
+			},
+    		resources = {
+    			["Wind_Kmeans_Spark.sh"] = {
+				file = OPERATOR_HOME .. "/Wind_Kmeans_Spark.sh",
+      				type = "file",               -- other value: 'archive'
+      				visibility = "application",  -- other values: 'private', 'public'
+    			},
+    			["archetipi.csv"] = {
+       				file = OPERATOR_HOME .. "/archetipi.csv",
+      				type = "file",               -- other value: 'archive'
+      				visibility = "application",  -- other values: 'private', 'public'
+    			},
+    			["clustering.py"] = {
+       				file = OPERATOR_HOME .. "/clustering.py",
+      				type = "file",               -- other value: 'archive'
+      				visibility = "application",  -- other values: 'private', 'public'
+    			}
+  			}		
+ 	}
+}
