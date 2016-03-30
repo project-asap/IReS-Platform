28bb628111709cac2bc2553f817e798920bc31dd
Papaioannou Vassilis <vpapa@cslab.ece.ntua.gr>
2016-03-30 21:00:53 +0300

    Update workflows

--- asap-platform/asap-server/asapLibrary/operators/Wind_Spatio_Temporal_Aggregation_Spark/Wind_Spatio_Temporal_Aggregation_Spark.sh
+++ asap-platform/asap-server/asapLibrary/operators/Wind_Spatio_Temporal_Aggregation_Spark/Wind_Spatio_Temporal_Aggregation_Spark.sh
@@ -26,5 +26,5 @@ $SPARK_HOME/bin/spark-submit --master $SPARK_PORT $OPERATOR $DATASET $IN_FILE $R
 ls -lah
 echo -e ""
 echo -e ""
-cat timeseriesroma-june-july-2015-aree_roma
+cat timeseriesroma-june-july-2015-aree_romacsv.csv
 echo -e "... spatio_temporal_aggregation.py script ended"
