727d1416f97160e8626d3eb5030d45357f4b0ca6
vpapaioannou <billiabon@yahoo.gr>
2016-07-20 13:12:17 +0300

    Update installation files

--- asap-platform/asap-server/asapLibrary/BasicLuaConf.lua
+++ asap-platform/asap-server/asapLibrary/BasicLuaConf.lua
@@ -2,7 +2,6 @@ IRES_HOME="/home/bill/PhD/projects/asap/asap4all/IReS-Platform"
 MASTER_JAR_LOCATION = IRES_HOME .. "/cloudera-kitten/java/master/target/kitten-master-0.2.0-jar-with-dependencies.jar"
 
 YARN_CLASSPATH="/home/bill/PhD/projects/yarn/etc/hadoop:/home/bill/PhD/projects/yarn/share/hadoop/common/lib/*:/home/bill/PhD/projects/yarn/share/hadoop/common/*:/home/bill/PhD/projects/yarn/share/hadoop/hdfs:/home/bill/PhD/projects/yarn/share/hadoop/hdfs/lib/*:/home/bill/PhD/projects/yarn/share/hadoop/hdfs/*:/home/bill/PhD/projects/yarn/share/hadoop/yarn/lib/*:/home/bill/PhD/projects/yarn/share/hadoop/yarn/*:/home/bill/PhD/projects/yarn/share/hadoop/mapreduce/lib/*:/home/bill/PhD/projects/yarn/share/hadoop/mapreduce/*:/contrib/capacity-scheduler/*.jar"
-OPERATOR_LIBRARY = "asapLibrary/operators"
 
 -- Resource and environment setup.
 base_resources = {
