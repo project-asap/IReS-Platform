eb5facddf310720c14c588292128e90d327a2d2c
Sofia Papagiannaki <spapagian@ics.forth.gr>
2016-11-04 11:26:45 +0200

    Support cross-origin addWorkflow requestis
    Annotate appropriately the addWorkflow method
    in order to support cross-origin requests.

--- asap-platform/asap-server/src/main/java/gr/ntua/cslab/asap/daemon/rest/AbstractWorkflows.java
+++ asap-platform/asap-server/src/main/java/gr/ntua/cslab/asap/daemon/rest/AbstractWorkflows.java
@@ -33,6 +33,7 @@ import javax.ws.rs.Consumes;
 import javax.ws.rs.DefaultValue;
 import javax.ws.rs.GET;
 import javax.ws.rs.POST;
+import javax.ws.rs.OPTIONS;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
@@ -45,48 +46,59 @@ import net.sourceforge.jeval.EvaluationException;
 
 import org.apache.log4j.Logger;
 
+import com.palominolabs.jersey.cors.Cors;
+import com.palominolabs.jersey.cors.CorsPreflight;
+
 @Path("/abstractWorkflows/")
 public class AbstractWorkflows {
 
 	@GET
+    @Cors(allowOrigin="*")  // Change it to specific hosts
 	@Produces("application/json")
 	@Path("{id}/")
     public WorkflowDictionary getDescription(@PathParam("id") String id) throws Exception {
         return AbstractWorkflowLibrary.getWorkflow(id, "<br>");
     }
 
-	
+
 	@GET
 	@Produces("application/XML")
 	@Path("XML/{id}/")
     public WorkflowDictionary getDescriptionXML(@PathParam("id") String id) throws Exception {
         return AbstractWorkflowLibrary.getWorkflow(id, "<br>");
     }
-	
 
-	
+
 	@POST
+    @Cors(allowOrigin="*")  // Change it to specific hosts
 	@Consumes({ MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON })
 	@Path("add/{id}/")
     public void addWorkflow(@PathParam("id") String id,
     		WorkflowDictionary workflow) throws Exception{
 		AbstractWorkflowLibrary.addWorkflow(id, workflow);
     }
-	
+
+	@OPTIONS
+    @CorsPreflight(allowHeaders="content-type", allowMethods="POST")
+	@Path("add/{id}/")
+    public void options() throws IOException, NumberFormatException, EvaluationException {
+        return;
+    }
+
 	@GET
 	@Produces("application/XML")
 	@Path("remove/{id}/")
     public void removeWorkflow(@PathParam("id") String id) throws IOException, NumberFormatException, EvaluationException {
         AbstractWorkflowLibrary.removeWorkflow(id);
     }
-	
+
 	@GET
 	@Produces("application/XML")
 	@Path("materialize/{id}/")
     public String materializeWorkflow(@PathParam("id") String id,@QueryParam("policy") String policy) throws Exception {
         return AbstractWorkflowLibrary.getMaterializedWorkflow(id, policy,"");
     }
-	
+
 	@GET
 	@Produces("application/XML")
 	@Path("materializeWithParams/{id}/")
