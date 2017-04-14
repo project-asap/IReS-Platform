500e99cbc0927e30ff329076885a978d857ef340
Sofia Papagiannaki <spapagian@ics.forth.gr>
2017-04-14 20:44:01 +0300

    Fix Swan operator

--- asap-platform/asap-server/defaultAsapLibrary/operators/WindLatestClusteringSwan/clustering.sh
+++ asap-platform/asap-server/defaultAsapLibrary/operators/WindLatestClusteringSwan/clustering.sh
@@ -3,6 +3,6 @@ export LD_LIBRARY_PATH=/home/asap/asap/qub/gcc-5/gcc-5.4.0-install/lib64:$LD_LIB
 # export PATH=$PATH:/home/forth/asap4all/qub/bin
 echo $@
 source /home/asap/virtualenvs/local/bin/activate
-python clustering_swan.py ${@:1}
+python clustering_swan.py ${@:3}
 # Just for testing the inputs/outputs to/from the operator:
 # hdfs dfs -copyFromLocal centroids/forth/2016_9 test_2016_9
